52f54d2ca213a085720faabd240697945a8ba801
João <gustavociscotto@gmail.com>
2025-08-22 11:09:22 -0300

    feat : main

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,8 +1,45 @@
 import styles from "./Main.module.css"
+import { CirclePlus } from "lucide-react"
+
 
 
 export function Main() {
     return <main className={styles.main}>
+
+        <div className={styles.tasks}>
+
+
+            <div className={styles.newTask}>
+                <input type="text" placeholder="Adicione uma nova tarefa" />
+
+                <button className={styles.buttonCreate}>
+                    <span>Criar</span><CirclePlus/>
+                </button>
+            </div>
+
+
+
+
+
+
+            <div className={styles.info}>
+                <div className={styles.tasksCreated}>
+                    <span>
+                        Tarefas criadas
+                    </span>
+
+                </div>
+
+                <div className={styles.tasksFinished}>
+                    <span>
+                        Concluidas
+                    </span>
+
+                </div>
+
+            </div>
+
+        </div>
 
     </main>
 }
