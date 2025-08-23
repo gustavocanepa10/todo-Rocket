11d42c66bac848325df0924f39347bfafc9642a0
João <gustavociscotto@gmail.com>
2025-08-23 19:33:24 -0300

    change : organizing some css and finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
-import { Header } from "./components/Header/Header"
-import { Main } from "./components/Main/Main"
-
-
+import { Header } from "./components/Header/Header";
+import { Main } from "./components/Main/Main";
 
 function App() {
-  
   return (
     <div>
-      <Header/>
+      <Header />
 
-      <Main/>
-        
-      
-
-      
+      <Main />
     </div>
-    
-  )
+  );
 }
 
-export default App
+export default App;
